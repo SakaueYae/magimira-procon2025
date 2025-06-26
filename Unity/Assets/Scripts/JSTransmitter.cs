246d2021d507d315f59fce8193d9202e6fa8be5b
SakaueYae <sky.bgm.tdrksht.132133@gmail.com>
2025-06-27 00:36:04 +0900

    add: Create SongWord Prefab

--- Unity/Assets/Scripts/JSTransmitter.cs
+++ Unity/Assets/Scripts/JSTransmitter.cs
@@ -1,12 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using R3;
 using TMPro;
 using UnityEngine;
 
-public class JSTransmitter : MonoBehaviour
+namespace GameData
 {
-    [SerializeField] TextMeshProUGUI _textMeshProUGUI;
-
-    public void OnWord(string word)
+    public class JSTransmitter : MonoBehaviour
     {
-        _textMeshProUGUI.text = word;
+        ReactiveProperty<string> _wordStream = new ReactiveProperty<string>();
+        public ReadOnlyReactiveProperty<string> WordStream()=> _wordStream;
+
+        // Test
+        void Start()
+        {
+            // Simulate receiving words every 2 seconds
+            UniTask.Void(async () =>
+            {
+                while (true)
+                {
+                    await UniTask.Delay(TimeSpan.FromSeconds(2));
+                    OnWord("Test Word " + DateTime.Now.ToString("HH:mm:ss"));
+                }
+            });
+        }
+
+        public void OnWord(string word)
+        {
+            _wordStream.Value = word;
+            Debug.Log("Word received: " + word);
+        }
     }
 }
---
+++ Unity/Assets/Scripts/SongWordController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace InGame
+{
+    public class SongWordController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            DelayAsync(destroyCancellationToken).Forget();
+        }
+
+        // 非同期メソッド
+        private async UniTask DelayAsync(CancellationToken token)
+        {
+            // 3秒間待つ
+            await UniTask.Delay(TimeSpan.FromSeconds(5), cancellationToken: token);
+
+            // 3秒後に原点にワープ
+            Destroy(gameObject);
+        }
+
+        public void SetWord(string word)
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().text = word;
+        }
+    }
+}
