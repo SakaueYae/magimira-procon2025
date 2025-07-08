73596acd4fb2f3ebf021e5b672a6974dddee7837
SakaueYae <sky.bgm.tdrksht.132133@gmail.com>
2025-07-08 11:48:54 +0900

    add: reset game scores when transit to GameScene

--- Unity/Assets/Scripts/InGame/ScoreData.cs
+++ Unity/Assets/Scripts/InGame/ScoreData.cs
@@ -1,5 +1,6 @@
 using R3;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace InGame.Scores
 {
@@ -22,10 +23,35 @@ namespace InGame.Scores
             {
                 instance = this;
                 DontDestroyOnLoad(gameObject);
+                
+                // シーン変更時のイベントを購読
+                SceneManager.sceneLoaded += OnSceneLoaded;
             }
             else
             {
                 Destroy(gameObject);
+            }
+        }
+
+        void OnDestroy()
+        {
+            // イベントの購読を解除（メモリリーク防止）
+            if (instance == this)
+            {
+                SceneManager.sceneLoaded -= OnSceneLoaded;
+            }
+        }
+
+        /// <summary>
+        /// シーンが読み込まれた時に呼ばれる
+        /// </summary>
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            // GameSceneに遷移した時にスコアをリセット
+            if (scene.name == "GameScene")
+            {
+                ResetScore();
+                Debug.Log("Score reset for GameScene");
             }
         }
 
@@ -37,6 +63,15 @@ namespace InGame.Scores
         public void SetMaxScore(int maxScore)
         {
             _maxscore = maxScore;
+        }
+
+        /// <summary>
+        /// スコアをリセットする
+        /// </summary>
+        public void ResetScore()
+        {
+            _score.Value = 0;
+            Debug.Log("Score has been reset to 0");
         }
     }
 
